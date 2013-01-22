834a333a684cf78705980d7f03553f5b723dd18b
jspears <speajus@gmail.com>
2013-01-22 10:43:05 -0500

    util for pretty printing json paths

--- prettyjs.js
+++ prettyjs.js
@@ -2,7 +2,7 @@
 var fs = require('fs');
 
 function depth(obj, str) {
-        if (!(obj && str)) return data;
+        if (!(obj && str)) return obj;
         var parts = str.split('.');
         while ((obj = obj[parts.shift()]) && parts.length);
         return obj;
@@ -17,15 +17,14 @@ function read(file, path){
             return;
 
         }
-
-
-
         data = JSON.parse(data);
-
-
         console.dir(depth(data, path));
         process.exit(0);
     });
 
 }
-read.apply(this, process.argv.slice(2));
+if (process.argv.length < 3){
+    console.log('usage: '+process.argv[1]+" <file> (path)")
+    process.exit(1);
+}else
+    read.apply(this, process.argv.slice(2));
